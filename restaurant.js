4b771c029ae886bb1f6033b85719e8fb863d12ef
Aitzi78 <aitzi7878@gmail.com>
2023-05-18 14:10:24 +0200

    Initial commit

---
+++ restaurant.js
@@ -0,0 +1,16 @@
+const menu = {
+    main: [
+        {name: "Pasta", price: 12},
+        {name: "Rissotto", price:14},
+    ],
+    side: [
+        {name: "Ensalada", price: 7},
+        {name: "Patatas", price: 6},
+    ],
+    comments: [
+        "Excente",
+        "Nuestra especialidad",
+    ],
+        
+    
+};
